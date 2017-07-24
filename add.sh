9b273ee5d0e8b6cc5eacc10d383aea16c58097b7
Emilio <zhiyong@augmedix.com>
2017-07-24 00:07:44 -0700

    correct time format of event commit

--- add.sh
+++ add.sh
@@ -2,6 +2,6 @@ cd /home/emilio/code/Reviewer
 java -cp /home/emilio/code/Reviewer/out/production/Reviewer/ core.EventAdd
 git status
 git add .
-dayTime = date +"%Y-%m-%d"
+dayTime=`date +%Y-%m-%d`
 git commit -m "add new events for $dayTime"
 git push
